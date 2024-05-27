41247d5d7b9ec4a5b05d7ccc0df8be980e1d9565
Shawn Morris <shawn@smorris.com>
2024-05-27 19:06:28 +0000

    adding

---
+++ mcc_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export cmds=$HOME/tmp/commands
+alias deploy1='clogin -x $cmds cfr01-$pop.hwng.net'
+alias deploy2='clogin -x $cmds cfr02-$pop.hwng.net'
+alias rdiff='diff -u ~/tmp/$pop-$peer-pre ~/tmp/$pop-$peer-post'
+alias build='python3 ~/src/mcc-yaml-test/geobgp_build.py -p $pop'
+rcpoint ()
+{
+    clogin -c "configure checkpoint save $1" $2
+}
+rsnap ()
+{
+    clogin -c "show ip bgp neigh $2 adv" cfr01-$pop.hwng.net > ~/tmp/$pop-$2-$1
+}
