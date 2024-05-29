e2e5de9575bf8e0a46b53dfab9c19717bfc07d02
Shawn Morris <shawn@smorris.com>
2024-05-29 23:36:46 +0000

    updating path to geobgp builder

--- mcc_setup.sh
+++ mcc_setup.sh
@@ -3,7 +3,7 @@ export cmds=$HOME/tmp/commands
 alias deploy1='clogin -x $cmds cfr01-$pop.hwng.net'
 alias deploy2='clogin -x $cmds cfr02-$pop.hwng.net'
 alias rdiff='diff -u ~/tmp/$pop-$peer-pre ~/tmp/$pop-$peer-post'
-alias build='python3 ~/src/mcc-yaml-test/geobgp_build.py -p $pop'
+alias build='python3 ~/src/geobgp/cfr_config/geobgp_cfr_build.py -p $pop
 rcpoint ()
 {
     clogin -c "configure checkpoint save $1" $2
